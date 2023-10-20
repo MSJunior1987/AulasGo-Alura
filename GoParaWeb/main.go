42fb5a057619c914e83ec2c1c0cac8bf386533fd
Márcio Junior de Souza <marcio.souza1987@live.com>
2023-10-20 13:27:59 -0300

    correcao da pagina index.html

--- GoParaWeb/main.go
+++ GoParaWeb/main.go
@@ -5,11 +5,15 @@ import (
 	"text/template"
 )
 
-var temp = template.Must(template.ParseGlob("templates/*.html")) //carrega todos as paginas html
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+
+	err := http.ListenAndServe(":8000", nil) //acessa com o endereco http://localhost/8000 no modo anonimo do chrome
+	if err != nil {
+		panic(err)
+	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
