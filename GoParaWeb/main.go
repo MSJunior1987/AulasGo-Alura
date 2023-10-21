e4eabd89f40c66520d975d1473361d4a3b596473
Márcio Junior de Souza <marcio.souza1987@live.com>
2023-10-21 16:57:14 -0300

    incluida conexao com o banco de dados mysql e incluido os arquivos .mod e .sum

--- GoParaWeb/main.go
+++ GoParaWeb/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func conectaComBancoDeDados() *sql.DB {
+	// conexao := ""
+	db, erro := sql.Open("mysql", "user:root@tcp(localhost:3306)/alura_loja")
+	if erro != nil {
+		panic(erro.Error())
+	}
+	return db
+}
 
 type Produto struct {
 	Nome, Descricao string
@@ -14,8 +26,10 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	http.HandleFunc("/", index)
 
+	db := conectaComBancoDeDados()
+	defer db.Close() //executa depois de tudo que executou no metodo conectaComBancoDeDados()
+	http.HandleFunc("/", index)
 	err := http.ListenAndServe(":8000", nil) //acessa com o endereco http://localhost/8000 no modo anonimo do chrome
 	if err != nil {
 		panic(err)
