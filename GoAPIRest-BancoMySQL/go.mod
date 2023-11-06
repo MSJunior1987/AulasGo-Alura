80b5b413a0e8061d09fb27cf1f8b382668aa4881
Márcio Junior de Souza <marcio.souza1987@live.com>
2023-11-06 17:18:48 -0300

    Substituido nome da pasta database para data.

--- GoAPIRest-BancoMySQL/go.mod
+++ GoAPIRest-BancoMySQL/go.mod
@@ -2,4 +2,5 @@ module main
 
 go 1.21.2
 
-require github.com/gorilla/mux v1.8.1 // indirect
+
+
--- GoAPIRest-BancoMySQL/main.go
+++ GoAPIRest-BancoMySQL/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"data"
 	"fmt"
 	"models"
 	"routes"
@@ -12,7 +13,7 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
-
+	data.ConectaComBancoDeDados()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
