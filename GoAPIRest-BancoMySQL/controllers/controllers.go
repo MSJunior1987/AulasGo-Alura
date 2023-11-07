b390b5d98aba2be44f63f56ae22b5efd8d39bb1b
Márcio Junior de Souza <marcio.souza1987@live.com>
2023-11-07 15:36:30 -0300

    Inclusao dos metodos de inclusao, alteracao e exclusao

--- GoAPIRest-BancoMySQL/controllers/controllers.go
+++ GoAPIRest-BancoMySQL/controllers/controllers.go
@@ -1,11 +1,11 @@
 package controllers
 
 import (
+	"data"
 	"encoding/json"
 	"fmt"
 	"models"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -15,16 +15,40 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalidades)
+	var p []models.Personalidade
+	data.DB.Find(&p) //retorna todas as personalidades do banco jogando na variavel p utilizando o endereco de memoria '&'
+	json.NewEncoder(w).Encode(p)
 }
 
 func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
+	var personalidade models.Personalidade
+	data.DB.First(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
 
-	for _, personalidade := range models.Personalidades {
-		if strconv.Itoa(personalidade.Id) == id {
-			json.NewEncoder(w).Encode(personalidade)
-		}
-	}
+func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
+	data.DB.Create(&novaPersonalidade)
+	json.NewEncoder(w).Encode(novaPersonalidade)
+}
+
+func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	data.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func EditaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	data.DB.First(&personalidade, id)              //retorna um registro pelo id
+	json.NewDecoder(r.Body).Decode(&personalidade) //decodifica o json retornado e converte em entidade de personalidade
+	data.DB.Save(&personalidade)                   //salva o novo registro de personalidade no banco
+	json.NewEncoder(w).Encode(personalidade)       //retorna um json com o valor novo
 }
